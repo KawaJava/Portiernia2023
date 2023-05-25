c03583355259ea8962af4db97ce85b113a262ef5
KawaJava <kawajava0550@gmail.com>
2023-05-25 17:21:52 +0200

    Email sending implemented

--- src/main/java/io/github/kawajava/portiernia2023/Portiernia2023Application.java
+++ src/main/java/io/github/kawajava/portiernia2023/Portiernia2023Application.java
@@ -2,8 +2,10 @@ package io.github.kawajava.portiernia2023;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Portiernia2023Application {
 
 	public static void main(String[] args) {
